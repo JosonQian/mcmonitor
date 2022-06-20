0e0eeae20ae1c32031deb465ba2affdd85675d46
李谦 <liqian9@asiainfo.com>
2022-06-20 16:54:39 +0800

    go monitor memcache test

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ var cmd *exec.Cmd
 
 func main() {
 	var out []byte
-	cmd = exec.Command("echo 'stats slabs'| nc 192.168.50.114 11211")
+	cmd = exec.Command("echo", "stats", "slabs", "|", "nc", "192.168.50.114", "11211")
 	out, err := cmd.Output()
 	if err != nil {
 		fmt.Println(err.Error())
