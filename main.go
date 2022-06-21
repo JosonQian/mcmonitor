442abf8d2ab63214e8adaebcf5dad9ea1c888410
李谦 <liqian9@asiainfo.com>
2022-06-21 12:22:59 +0800

    go monitor memcache test

--- main.go
+++ main.go
@@ -9,12 +9,11 @@ import (
 	"os/exec"
 	"strconv"
 	"strings"
-	"time"
 )
 
 var pages_count, chunk_count, use_chunk, free_chunk int
 
-func getMcSlabs() {
+func GetMcSlabs() {
 	var cmd *exec.Cmd
 	var out []byte
 	cmd = exec.Command("bash", "-c", "echo 'stats slabs'|nc 127.0.0.1 11211 |grep -E 'total_pages|total_chunks|free_chunks'|grep -v free_chunks_end|awk -F ':' '{print $2}'")
@@ -46,30 +45,54 @@ func getMcSlabs() {
 		fmt.Println(pages_count, chunk_count, use_chunk, free_chunk)
 	}
 }
-func recordMetrics(c *prometheus.GaugeVec) {
-	go func() {
-		for {
-			c.With(prometheus.Labels{"total_pages": "Red"}).Set(float64(pages_count)) // 写入指标
-			c.With(prometheus.Labels{"total_chunk": "Red"}).Set(float64(chunk_count))
-			c.With(prometheus.Labels{"use_chunk": "Red"}).Set(float64(use_chunk))
-			c.With(prometheus.Labels{"free_chunk": "Red"}).Set(float64(free_chunk))
-			time.Sleep(time.Second * 2)
-		}
-	}()
-}
 
-func main() {
-	MyGauge := prometheus.NewGaugeVec(prometheus.GaugeOpts{ // 定义指标
-		Name: "test_gauge",
+func Exporter() {
+	//get total pages
+	gaugeTotalPages := prometheus.NewGauge(prometheus.GaugeOpts{ // 定义指标
+		Name: "pages_count",
 		Help: "Test Gauge Custom help info",
-	},
-		[]string{"total_pages", "total_chunk", "use_chunk", "free_chunk"},
-	)
-
-	if err := prometheus.Register(MyGauge); err != nil {
+	})
+	if err := prometheus.Register(gaugeTotalPages); err != nil {
 		log.Fatal(err)
 	}
-	recordMetrics(MyGauge)
+	gaugeTotalPages.Set(float64(pages_count))
+
+	//get total chunks
+	gaugeTotalChunks := prometheus.NewGauge(prometheus.GaugeOpts{ // 定义指标
+		Name: "chunk_count",
+		Help: "Test Gauge Custom help info",
+	})
+	if err := prometheus.Register(gaugeTotalChunks); err != nil {
+		log.Fatal(err)
+	}
+	gaugeTotalChunks.Set(float64(chunk_count))
+
+	//get use chunks
+	gaugeUseChunks := prometheus.NewGauge(prometheus.GaugeOpts{ // 定义指标
+		Name: "use_chunk",
+		Help: "Test Gauge Custom help info",
+	})
+	if err := prometheus.Register(gaugeUseChunks); err != nil {
+		log.Fatal(err)
+	}
+	use_chunk = chunk_count - free_chunk
+	gaugeTotalPages.Set(float64(use_chunk))
+
+	//get free chunks
+	gaugeFreeChunks := prometheus.NewGauge(prometheus.GaugeOpts{ // 定义指标
+		Name: "free_chunk",
+		Help: "Test Gauge Custom help info",
+	})
+	if err := prometheus.Register(gaugeFreeChunks); err != nil {
+		log.Fatal(err)
+	}
+	gaugeTotalPages.Set(float64(free_chunk))
+
+}
+func main() {
+	GetMcSlabs()
+	Exporter()
+
 	http.Handle("/metrics", promhttp.Handler()) // 服务！
 	log.Fatal(http.ListenAndServe("0.0.0.0:9109", nil))
 }
